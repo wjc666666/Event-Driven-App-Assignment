436c64b694f648dfdc6dfcf93389af854638d595
wjc666666 <2101079563@qq.com>
2025-04-27 21:39:31 +0100

    Initial commit: CDK project setup

---
+++ test/event-driven app assignment.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EventDrivenAppAssignment from '../lib/event-driven app assignment-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/event-driven app assignment-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EventDrivenAppAssignment.EventDrivenAppAssignmentStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
